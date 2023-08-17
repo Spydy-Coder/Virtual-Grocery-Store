0ea6ab390a1cc46fe2e050cfd64d0dd7f57979de
Spydy-Coder <81745446+Spydy-Coder@users.noreply.github.com>
2023-08-17 15:33:20 +0530

    login page design

--- frontend/src/features/auth/components/Login.js
+++ frontend/src/features/auth/components/Login.js
@@ -3,6 +3,7 @@ import { selectError, selectLoggedInUser } from '../authSlice';
 import { Link, Navigate } from 'react-router-dom';
 import { checkUserAsync } from '../authSlice';
 import { useForm } from 'react-hook-form';
+import logo from './shop.png';
 
 export default function Login() {
   const dispatch = useDispatch();
@@ -17,12 +18,14 @@ export default function Login() {
 
   return (
     <>
+    
       {user && <Navigate to="/" replace={true}></Navigate>}
+      <div class="bg-[#b2e269] shadow-md rounded h-128 w-1/2  absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 ">
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm" >
           <img
             className="mx-auto h-10 w-auto"
-            src="/ecommerce.png"
+            src={logo}
             alt="Your Company"
           />
           <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
@@ -58,7 +61,7 @@ export default function Login() {
                     },
                   })}
                   type="email"
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-green-600 sm:text-sm sm:leading-6"
                 />
                 {errors.email && (
                   <p className="text-red-500">{errors.email.message}</p>
@@ -67,7 +70,7 @@ export default function Login() {
             </div>
 
             <div>
-              <div className="flex items-center justify-between">
+              <div className="flex justify-between flex-col sm:flex-row">
                 <label
                   htmlFor="password"
                   className="block text-sm font-medium leading-6 text-gray-900"
@@ -77,7 +80,7 @@ export default function Login() {
                 <div className="text-sm">
                   <Link
                     to="/forgot-password"
-                    className="font-semibold text-indigo-600 hover:text-indigo-500"
+                    className="font-semibold text-black-600 hover:text-red-500"
                   >
                     Forgot password?
                   </Link>
@@ -102,23 +105,24 @@ export default function Login() {
             <div>
               <button
                 type="submit"
-                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                className="flex w-full justify-center rounded-md bg-green-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-[#408000] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600"
               >
                 Log in
               </button>
             </div>
           </form>
 
-          <p className="mt-10 text-center text-sm text-gray-500">
+          <p className="mt-10 text-center text-sm text-black-500">
             Not a member?{' '}
             <Link
               to="/signup"
-              className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+              className="font-semibold leading-6 text-white-600 hover:text-red-500"
             >
               Create an Account
             </Link>
           </p>
         </div>
+      </div>
       </div>
     </>
   );
--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,7 +1,11 @@
 import Login from "../features/auth/components/Login";
 function LoginPage() {
     return ( <div>
+    
+    <div className="bg-contain h-screen"
+  style={{ backgroundImage: "url('https://media.istockphoto.com/id/1198380802/vector/food-background-vegetables-seamless-pattern-healthy-eating-tomato-garlic-carrot-pepper.jpg?s=612x612&w=0&k=20&c=jHB3uTWZrakPyAgqSQfOApxSq_jIwHRx00LiOV2YHEk=')"}}>
         <Login></Login>
+        </div>
     </div>  );
 }
 
